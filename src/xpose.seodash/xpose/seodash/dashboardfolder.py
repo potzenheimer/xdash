8762b4acfdb476b5bce29c5d3ce6f36a003e5c1b
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-06-10 16:53:59 +0200

    Add profile selection view

--- src/xpose.seodash/xpose/seodash/dashboardfolder.py
+++ src/xpose.seodash/xpose/seodash/dashboardfolder.py
@@ -3,12 +3,15 @@ from Acquisition import aq_inner
 from AccessControl import Unauthorized
 from five import grok
 from plone import api
+from zope.lifecycleevent import modified
 
 from zope.component import getMultiAdapter
 from plone.directives import form
 from plone.keyring import django_random
 from plone.dexterity.content import Container
 from Products.CMFPlone.utils import safe_unicode
+
+from Products.statusmessages.interfaces import IStatusMessage
 
 from xpose.seodash.dashboard import IDashboard
 from xpose.seotool.seotool import ISeoTool
@@ -52,6 +55,37 @@ class ManageDashboards(grok.View):
 
     def update(self):
         self.has_dashboards = len(self.dashboards()) > 0
+        context = aq_inner(self.context)
+        self.errors = {}
+        unwanted = ('_authenticator', 'form.button.Submit')
+        required = ('ga.profile')
+        if 'form.button.Submit' in self.request:
+            authenticator = getMultiAdapter((context, self.request),
+                                            name=u"authenticator")
+            if not authenticator.verify():
+                raise Unauthorized
+            form = self.request.form
+            form_data = {}
+            form_errors = {}
+            errorIdx = 0
+            for value in form:
+                if value not in unwanted:
+                    form_data[value] = safe_unicode(form[value])
+                    if not form[value] and value in required:
+                        error = {}
+                        error['active'] = True
+                        error['msg'] = _(u"This field is required")
+                        form_errors[value] = error
+                        errorIdx += 1
+                    else:
+                        error = {}
+                        error['active'] = False
+                        error['msg'] = form[value]
+                        form_errors[value] = error
+            if errorIdx > 0:
+                self.errors = form_errors
+            else:
+                self._update_dashboard(form)
 
     def has_token(self):
         token = False
@@ -75,6 +109,18 @@ class ManageDashboards(grok.View):
         item = portal['adm']
         stored = getattr(item, 'projects_ga')
         return json.loads(stored)
+
+    def _update_dashboard(self, data):
+        context = aq_inner(self.context)
+        setattr(context, 'ga_id', data['ga.profile'])
+        modified(context)
+        context.reindexObject(idxs='modified')
+        IStatusMessage(self.request).addStatusMessage(
+            _(u"GA configuration has sucessfully been refreshed"),
+            type='info')
+        portal_url = api.portal.get().absolute_url()
+        url = '{0}/adm/@@setup-google'.format(portal_url)
+        return self.request.response.redirect(url)
 
     def can_edit(self):
         context = aq_inner(self.context)
